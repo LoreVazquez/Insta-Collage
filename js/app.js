09819799951a75eef84773bf77e425874b4de4d8
LoreVazquez <lorena_vor@hotmail.com>
2018-07-30 13:34:26 -0500

    Seccion de platillas y eliminar fotos, archivo de touch

--- js/app.js
+++ js/app.js
@@ -21,6 +21,13 @@ const onDrop = (e) => {
 
 };
 
+const deleteDrop = (e) =>{
+    e.preventDefault();
+    let data = e.dataTransfer.getData('text');
+    let element = document.getElementById(data);
+    element.parentNode.removeChild(element);
+};
+
 //Evento para agregar fotos
 const addPicture = (e) => {
     let file = e.target.files[0];
@@ -40,8 +47,95 @@ const addPicture = (e) => {
 };
 
 const selectTemplate = () => {
+    let valueSelect = document.getElementById('template').value;
+    let container = document.getElementById("html-content-holder");
+    
+    if(valueSelect == 3){
+        let templateThree = '<div class="row">'+
+                            '<div class="col p-frame">'+
+                                '<div class="row">'+
+                                    '<div class="col frame n-pad img-thumbnail"></div>'+
+                                '</div>'+
+                                '<div class="row">'+
+                                    '<div class="col frame n-pad img-thumbnail"></div>'+
+                                '</div>'+
+                            '</div>'+
+                            '<div class="col p-frame n-pad img-thumbnail"></div>'+
+                        '</div>';
+        container.innerHTML = templateThree;
+    } else if( valueSelect == 4){
+        let templateFour = `<div class="row">
+                                <div class="col p-frame">
+                                    <div class="row">
+                                        <div class="col frame n-pad img-thumbnail"></div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col frame n-pad img-thumbnail"></div>
+                                    </div>
+                                </div>
+                                <div class="col p-frame n-pad img-thumbnail">
+                                    <div class="row">
+                                        <div class="col frame n-pad img-thumbnail"></div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col frame n-pad img-thumbnail"></div>
+                                    </div>
+                                </div>
+                            </div>`;
 
-}
+        container.innerHTML = templateFour;
+
+    }else if( valueSelect == 5){
+        let templateFive = `<div class="row">
+                                <div class="col-4 p-frame">
+                                    <div class="row">
+                                        <div class="col frame n-pad img-thumbnail rounded-circle"></div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col frame n-pad img-thumbnail"></div>
+                                    </div>
+                                </div>
+                                <div class="col p-frame n-pad img-thumbnail">
+                                    <div class="row">
+                                        <div class="col frame n-pad img-thumbnail"></div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col frame n-pad">
+                                            <div class="row">
+                                                <div class="col-7 frame n-pad img-thumbnail rounded-right"></div>
+                                                <div class="col frame n-pad img-thumbnail rounded-left"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>`;
+
+        container.innerHTML = templateFive;
+
+    }else if(valueSelect == 6){
+        let templateSix = `<div class="row">
+                                <div class="col-4 p-frame n-pad img-thumbnail">
+                                </div>
+                                <div class="col p-frame n-pad img-thumbnail">
+                                    <div class="row">
+                                        <div class="col frame n-pad img-thumbnail"></div>
+                                        <div class="col frame n-pad img-thumbnail"></div>
+                                        <div class="col frame n-pad img-thumbnail"></div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col frame n-pad">
+                                            <div class="row">
+                                                <div class="col-7 frame n-pad img-thumbnail"></div>
+                                                <div class="col frame n-pad img-thumbnail"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>`;
+
+        container.innerHTML = templateSix;
+    }
+};
 
 const deleteItem = () => {
     let element = document.getElementById('myCanvas');
@@ -71,6 +165,7 @@ const init = () => {
     let inputFile = document.getElementById('inputFile');
     let deleteCollage = document.getElementById('deleteCollage');
     let template = document.getElementById('template');
+    let trash = document.getElementById('trash');
 
     container.addEventListener('dragover', dragOver);
     container.addEventListener('drop', onDrop);
@@ -80,7 +175,10 @@ const init = () => {
 
     inputFile.addEventListener('change', addPicture);
     deleteCollage.addEventListener('click', deleteItem);
-    template.addEventListener('change', selectTemplate)
+    template.addEventListener('change', selectTemplate);
+
+    trash.addEventListener('dragover', dragOver);
+    trash.addEventListener('drop', deleteDrop);
 };
 
 window.addEventListener('load', init);
